f6d01f274951d6bddfe2e539297e5c81f385ebd2
Pedro Lucas <ppedrolangelo@gmail.com>
2023-07-18 15:44:27 -0300

    Adicionando erro caso so tenha preenchido uma data

--- src/formulario.js
+++ src/formulario.js
@@ -19,34 +19,55 @@ function Formulario(){
         setFiltro({... objFiltro, [e.target.name]:e.target.value})
     };
 
+    const validarFormulario = () => {
+
+        if (objFiltro.dataInicial && !objFiltro.dataFinal) {
+          setErrorMessage('Por favor, preencha a Data Final');
+          return false;
+        }
+    
+        if (!objFiltro.dataInicial && objFiltro.dataFinal) {
+          setErrorMessage('Por favor, preencha a Data Inicial');
+          return false;
+        }
+    
+        return true;
+      };
+
     const pesquisarTransferencias = () => {
-        if (objFiltro.dataInicial) {
-            queryParams.append("dataInicial", objFiltro.dataInicial);
+        if(validarFormulario()){
+            setErrorMessage("")
+            if (objFiltro.dataInicial) {
+                queryParams.append("dataInicial", objFiltro.dataInicial);
+            }
+            
+            if (objFiltro.dataFinal) {
+                queryParams.append("dataFinal", objFiltro.dataFinal);
+            }
+               
+              if (objFiltro.nome) {
+                queryParams.append("nome", objFiltro.nome);
+            }
+            const url = `${baseUrl}?${queryParams}`;
+            fetch(url)
+            .then(retorno => retorno.json())
+            .then(retorno_convertido => setTransferencia(retorno_convertido))
+            .catch(error => {
+                setErrorMessage(error.message);
+            });
         }
-        
-        if (objFiltro.dataFinal) {
-            queryParams.append("dataFinal", objFiltro.dataFinal);
-        }
-           
-          if (objFiltro.nome) {
-            queryParams.append("nome", objFiltro.nome);
-        }
-        const url = `${baseUrl}?${queryParams}`;
-        console.log(url)
-        fetch(url)
-        .then(retorno => retorno.json())
-        .then(retorno_convertido => setTransferencia(retorno_convertido))
-        .catch(error => {
-            setErrorMessage(error.message); // Armazenar a mensagem de erro no estado
-          });
     }
 
     return(
         <>
             <form>
                 <div className='box'>
-                    <input type='date' onChange={digitar} name="dataInicial" placeholder='Data Inicio' className='form-control'/>
-                    <input type='date' onChange={digitar} name="dataFinal" placeholder='Data Fim' className='form-control'/>
+                    <input type='date' onChange={digitar} name="dataInicial" placeholder='Data Inicio' 
+                        className={`form-control ${!objFiltro.dataInicial && objFiltro.dataFinal ? 'is-invalid' : ''}`}
+                    />
+                    <input type='date' onChange={digitar} name="dataFinal" placeholder='Data Fim' 
+                        className={`form-control ${objFiltro.dataInicial && !objFiltro.dataFinal ? 'is-invalid' : ''}`}
+                    />
                     <input type='text' onChange={digitar} name="nome" placeholder='Nome do Operador Transicionado' className='form-control'/>
                 </div>
                 
