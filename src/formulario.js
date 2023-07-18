4f77cfd40d0c2075202df4bf20c69785037eac08
Pedro Lucas <ppedrolangelo@gmail.com>
2023-07-18 14:40:35 -0300

    Alterando tipo de data inicial e data final p date

--- src/formulario.js
+++ src/formulario.js
@@ -5,6 +5,7 @@ function Formulario(){
     const [transferencia, setTransferencia] = useState([]);
     const baseUrl = "http://localhost:8080/transferencias"
     const queryParams = new URLSearchParams();
+    const [errorMessage, setErrorMessage] = useState('');
 
     const filtros = {
         dataInicial : null,
@@ -20,12 +21,10 @@ function Formulario(){
 
     const pesquisarTransferencias = () => {
         if (objFiltro.dataInicial) {
-          console.log(objFiltro.dataInicial)
-          queryParams.append("dataInicial", objFiltro.dataInicial);
+            queryParams.append("dataInicial", objFiltro.dataInicial);
         }
         
         if (objFiltro.dataFinal) {
-            console.log(objFiltro.dataFinal)
             queryParams.append("dataFinal", objFiltro.dataFinal);
         }
            
@@ -36,18 +35,22 @@ function Formulario(){
         console.log(url)
         fetch(url)
         .then(retorno => retorno.json())
-        .then(retorno_convertido => setTransferencia(retorno_convertido));
+        .then(retorno_convertido => setTransferencia(retorno_convertido))
+        .catch(error => {
+            setErrorMessage(error.message); // Armazenar a mensagem de erro no estado
+          });
     }
 
     return(
         <>
             <form>
                 <div className='box'>
-                    <input type='text' onChange={digitar} name="dataInicial" placeholder='Data Inicio' className='form-control'/>
-                    <input type='text' onChange={digitar} name="dataFinal" placeholder='Data Fim' className='form-control'/>
+                    <input type='date' onChange={digitar} name="dataInicial" placeholder='Data Inicio' className='form-control'/>
+                    <input type='date' onChange={digitar} name="dataFinal" placeholder='Data Fim' className='form-control'/>
                     <input type='text' onChange={digitar} name="nome" placeholder='Nome do Operador Transicionado' className='form-control'/>
                 </div>
-
+                
+                {errorMessage && <p>{errorMessage}</p>} {/* Mostrar a mensagem de erro se existir */}
                 <input type='button' onClick={pesquisarTransferencias} value='Pesquisar' className='btn btn-primary'/>
             </form>
             <Tabela vetor={transferencia} itensPorPagina={4}/>
